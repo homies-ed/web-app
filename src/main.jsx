a7e8543eade8fea28eafe9b779c1d8c3c4610904
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-12 13:16:57 +0200

    Initialising routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import ReactDOM from 'react-dom/client';
-import './style/style.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Start from './page/Start';
+import Zbieranina from './page/Zbieranina';
+import Login from './page/Login';
+import SignUp from './page/SignUp';
+import Privacy from './page/Privacy';
+import Terms from './page/Terms';
+import Settings from './page/Settings';
+import PasswordRecovery from './page/PasswordRecovery';
+import Planer from './page/Planer';
+import Topics from './page/Topics';
+import Documents from './page/Documents';
+
+import reportWebVitals from './reportWebVitals';
+import './style/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Routes></Routes>
+    <Routes>
+      <Route path='/' element={<Start />} />
+      <Route path='/logowanie' element={<Login />} />
+      <Route path='/rejestracja' element={<SignUp />} />
+      <Route path='/zagadnienie' element={<Documents />} />
+      <Route path='/planer' element={<Planer />} />
+      <Route path='/ustawienia' element={<Settings />} />
+      <Route path='/regulamin' element={<Terms />} />
+      <Route path='/polityka-prywatnosci' element={<Privacy />} />
+      <Route path='/odzyskaj-haslo' element={<PasswordRecovery />} />
+      <Route path='/zbieranina' element={<Zbieranina />} />
+      <Route path='/zbieranina/polski' element={<Topics subject='Język polski' />} />
+      <Route path='/zbieranina/angielski' element={<Topics subject='Język angielski' />} />
+      <Route path='/zbieranina/matematyka' element={<Topics subject='Matematyka' />} />
+    </Routes>
   </Router>
 );
 
