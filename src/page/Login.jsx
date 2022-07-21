3bf3e9c7c98f08732f72f289048a074f9d0c6185
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-21 18:50:46 +0200

    Coloring start and login pages

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { GoogleAuthProvider, getAuth, signInWithPopup, FacebookAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, setPersistence, browserSessionPersistence } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 
-import VisibilityIcon from '../icon/VisibilityIcon';
 import Logo from '../component/Logo';
+import PasswordInput from '../component/PasswordInput';
 
 const auth = getAuth();
 auth.languageCode = 'pl';
@@ -11,35 +11,11 @@ auth.languageCode = 'pl';
 const emailLogin = () => {
 	const email = document.getElementById('email-login').value;
 	const password = document.getElementById('password-login').value;
-
-	setPersistence(auth, browserSessionPersistence)
-		.then(() => {
-			signInWithEmailAndPassword(auth, email, password);
-			return true;
-		})
-		.catch((error) => console.error(error.message));
-
-	return false;
-}
-
-const googleLogin = () => {
-	const provider = new GoogleAuthProvider();
-	signInWithPopup(auth, provider)
-		.catch((error) => console.error(error.message));
-}
-
-const facebookLogin = () => {
-	const provider = new FacebookAuthProvider();
-	signInWithPopup(auth, provider)
-		.catch((error) => console.error(error));
+	signInWithEmailAndPassword(auth, email, password);
 }
 
 const Login = () => {
 	const navigate = useNavigate();
-
-	const handleLogin = () => {
-		if (emailLogin() || googleLogin() || facebookLogin()) navigate('/zbieranina');
-	}
 
 	onAuthStateChanged(auth, (user) => {
 		if (user) navigate('/zbieranina');
@@ -52,12 +28,10 @@ const Login = () => {
 				<form action='' method='' className='form'>
 					<input type='email' id='email-login' className='input' placeholder='Adres email' tabIndex='0' required />
 					<div className='password-input'>
-						<div className='input password-bar'>
-							<input type='password' id='password-login' className='password-login' placeholder='Hasło' tabIndex='0' required /><VisibilityIcon />
-						</div>
+						<PasswordInput />
 						<p className='password-tooltip' tabIndex='0'><Link to='/odzyskaj-haslo' className='link tooltip'>Nie pamiętasz hasła?</Link></p>
 					</div>
-					<input type='button' value='Zaloguj się' id='button-login' className='button login-button link' onClick={handleLogin} required />
+					<input type='button' value='Zaloguj się' id='button-login' className='button login-button link' required />
 				</form>
 			</article>
 			<section className='alt-login'>
--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -3,6 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile, onAuthStateChan
 import { validateName, validateEmail, validatePassword, validateCredentials } from '../service/module';
 
 import Logo from '../component/Logo';
+import PasswordInput from '../component/PasswordInput';
 
 const auth = getAuth();
 
@@ -44,13 +45,13 @@ const SignUp = () => {
         <input type='text' id='first-name-signup' className='input' placeholder='Imię' tabIndex='0' required/>
         <input type='text' id='last-name-signup' className='input' placeholder='Nazwisko' tabIndex='0' required/>
         <input type='email' id='email-sign-up' className='input' placeholder='Adres email' tabIndex='0' required/>
-        <input type='password' id='password-sign-up' className='input' placeholder='Hasło' tabIndex='0' required/>
+        <PasswordInput />
         <section className='confirmation'>
           <span className='checkbox-container'>
             <input type='checkbox' id='checkbox-signup' className='checkbox' tabIndex='0'/>
             <label htmlFor='checkbox-signup' className='checkbox-label'>Akceptuję <Link to='/regulamin' className='link'>regulamin</Link> i <Link to='/polityka-prywatnosci' className='link'>politykę prywatności</Link> HOMIES.</label>
           </span>  
-          <button className='button' type='button' tabIndex='0' onClick={getCredentials}>Utwórz konto</button>
+          <button className='button' type='button' tabIndex='0'>Utwórz konto</button>
         </section>
       </form>
     </article>
