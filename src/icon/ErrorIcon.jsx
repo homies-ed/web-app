b4f0ae80805c850935178b7d1bd39056616699ad
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-21 18:51:12 +0200

    Adding error state icon

---
+++ src/icon/ErrorIcon.jsx
@@ -0,0 +1,10 @@
+const ErrorIcon = () => {
+  return (
+    <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="15.5" cy="15.5" r="15.5" fill="#F2542D" />
+      <rect x="4.09375" y="13.4529" width="23.3962" height="4.67925" rx="2.33962" fill="white" />
+    </svg>
+  );
+}
+
+export default ErrorIcon;
