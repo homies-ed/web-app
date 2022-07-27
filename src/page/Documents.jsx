7ea5bf4eba15a72cad9750aa81d60d559905c1aa
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-27 10:49:53 +0200

    Coloring topics and documents

--- src/page/Documents.jsx
+++ src/page/Documents.jsx
@@ -2,19 +2,44 @@ import Menu from "../component/Menu";
 import Document from "../component/Document";
 import BackIcon from "../icon/BackIcon";
 
-const Documents = () => {
+const displayTopics = (subject) => {
+    try {
+        let array = [];
+
+        const polishTopics = ["Młoda Polska", "Średniowiecze", "Homer", "Horacy", "Jan Kochanowski", "Zbrodnia i kara"];
+        const mathTopics = ["Funkcja liniowa", "Trygonometria", "Rachunek prawdopodobieństwa", "Zdarzenia losowe", "Równania i nierówności", "Logika"];
+        const englishTopics = ["Czasy przyszłe", "Konstrukcje warunkowe", "Strona bierna", "False friends", "Idiomy", "Poprawna wymowa"];
+        const tags = ["notatka, do 10 minut", "artykuł, do 20 minut", "podcast, do 90 minut", "notatka, do 30 minut", "film, do 180 minut", "serial, do 360 minut"];
+
+        let source = [];
+        switch (subject) {
+            case "Język polski":
+                source = [...polishTopics];
+                break;
+
+            case "Matematyka":
+                source = [...mathTopics];
+                break;
+
+            case "Język angielski":
+                source = [...englishTopics];
+                break;
+        }
+
+        for (let i = 0; i < 6; i += 1) array[i] = <Document name={source[i]} key={i} tags={tags[i]}/>;
+        return array;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const Documents = (props) => {
     return (
         <section className="documents">
             <BackIcon class="back-zbieranina" />
             <h1 className="documents-heading">Temat</h1>
             <section className="documents-container">
-                <Document name="Materiał" tags="notatka, do 10 minut" />
-                <Document name="Materiał" tags="artykuł, do 20 minut" />
-                <Document name="Materiał" tags="podkast, do 60 minut" />
-                <Document name="Materiał" tags="podkast, do 90 minut" />
-                <Document name="Materiał" tags="notatka, do 30 minut" />
-                <Document name="Materiał" tags="film, do 180 minut" />
-                <Document name="Materiał" tags="serial, do 360 minut" />
+                {displayTopics("Język polski")}
             </section>
             <Menu />
         </section>
--- src/page/Topics.jsx
+++ src/page/Topics.jsx
@@ -1,11 +1,34 @@
 import BackIcon from "../icon/BackIcon";
 import Menu from "../component/Menu";
 import SmallTile from "../component/SmallTile";
+import Novelist from "../icon/Novelist";
+import Bibliophile from "../icon/Bibliophile";
+import Mathematics from "../icon/Mathematics";
 
-const displayTopics = (quantity) => {
+const displayTopics = (subject) => {
   try {
     let array = [];
-    for (let i = 0; i < quantity; i += 1) array[i] = <SmallTile name="Temat" key={i} />;
+
+    const polishTopics = ["Młoda Polska", "Średniowiecze", "Homer", "Horacy", "Jan Kochanowski"];
+    const mathTopics = ["Funkcja liniowa", "Trygonometria", "Rachunek prawdopodobieństwa", "Zdarzenia losowe", "Równania i nierówności"];
+    const englishTopics = ["Czasy przyszłe", "Konstrukcje warunkowe", "Strona bierna", "False friends", "Idiomy"];
+
+    let source = [];
+    switch(subject) {
+      case "Język polski":
+        source = [...polishTopics];
+      break;
+
+      case "Matematyka":
+        source = [...mathTopics];
+      break;
+
+      case "Język angielski":
+        source = [...englishTopics];
+      break;
+    }
+
+    for (let i = 0; i < 5; i += 1) array[i] = <SmallTile name={source[i]} key={i} />;
     return array;
   } catch (error) {
     console.error(error);
@@ -13,12 +36,30 @@ const displayTopics = (quantity) => {
 }
 
 const Topics = (props) => {
+  let illustration;
+
+  switch (props.subject) {
+    case "Język polski":
+      illustration = <Novelist />;
+    break;
+
+    case "Język angielski":
+      illustration = <Bibliophile />
+    break;
+
+    case "Matematyka":
+      illustration = <Mathematics />
+    break;
+  }
+  
   return (
     <article className="topics">
       <BackIcon class="back-zbieranina"/>
       <h1 className="topics-heading">{props.subject}</h1>
-      <h2 className="tertiary-heading">Działy</h2>
-      {displayTopics(5)}      
+      {illustration}
+      <section className="topic-list">
+        {displayTopics(props.subject)}
+      </section>
       <Menu />
     </article>
   );
