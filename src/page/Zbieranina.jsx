bfe7bbf989e9e40d5efcf45b270830f2b13aec38
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-07-30 15:01:27 +0200

    Rejecting unauthorised users from Zbieranina

--- src/page/Zbieranina.jsx
+++ src/page/Zbieranina.jsx
@@ -2,11 +2,15 @@ import { getAuth } from "firebase/auth";
 import Menu from "../component/Menu";
 import Tile from "../component/Tile";
 import SmallTile from "../component/SmallTile";
+import ErrorModal from "../component/ErrorModal";
 
 const rejectAccess = () => {
-	if (getAuth().currentUser) return;
-	document.body.style.opacity = ".5";
-	document.body.style.pointerEvents = "none";
+	try {
+		if (getAuth().currentUser) return false;
+		else return true;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 const getUserName = () => {
@@ -17,27 +21,33 @@ const getUserName = () => {
 	}
 }
 
+// mock data 
 const getSubject = () => {
-	const subject = localStorage.getItem("subject");
+	try {
+		const subject = localStorage.getItem("subject");
 
-	switch (subject) {
-		case "polish":
-			return "Język polski";
+		switch (subject) {
+			case "polish":
+				return "Język polski";
 
-		case "math":
-			return "Matematyka";
+			case "math":
+				return "Matematyka";
 
-		case "english":
-			return "Język angielski";
+			case "english":
+				return "Język angielski";
+		}
+	} catch (error) {
+		console.error(errror);
 	}
 }
 
+// mock data 
 const displayTopics = (subject) => {
 	try {
 		let array = [];
 
 		const polishTopics = ["Młoda Polska", "Średniowiecze", "Homer", "Horacy", "Jan Kochanowski", "Zbrodnia i kara"];
-		const mathTopics = ["Funkcja liniowa", "Trygonometria", "Rachunek prawdopodobieństwa", "Zdarzenia losowe", "Równania i nierówności", "Logika"];
+		const mathTopics = ["Funkcja liniowa", "Trygonometria", "Prawdopodobieństwo", "Zdarzenia losowe", "Równania i nierówności", "Logika"];
 		const englishTopics = ["Czasy przyszłe", "Konstrukcje warunkowe", "Strona bierna", "False friends", "Idiomy", "Poprawna wymowa"];
 		const tags = ["notatka, do 10 minut", "artykuł, do 20 minut", "podcast, do 90 minut", "notatka, do 30 minut", "film, do 180 minut", "serial, do 360 minut"];
 
@@ -64,24 +74,24 @@ const displayTopics = (subject) => {
 }
 
 const Zbieranina = () => {
-	rejectAccess();
-
 	return (
-		<article className="zbieranina">
-			<h1 className="secondary-heading zbieranina-heading" tabIndex="0">
-				{getUserName() || <><span className="greeting">Cześć!</span><br/></>}
-				<span className="subheading">Czego chcesz się dzisiaj nauczyć?</span>
-			</h1>
-			<h2 className="tertiary-heading">Przedmioty</h2>
-			<article className="subject-tiles">
-				<Tile name="Polski" />
-				<Tile name="Matematyka" />
-				<Tile name="Angielski" />
-			</article>
-			<h2 className="tertiary-heading">Zagadnienia z ostatniego przedmiotu</h2>
+		<>{ rejectAccess() ? <ErrorModal /> : 
+			<article className="zbieranina">
+				<h1 className="secondary-heading zbieranina-heading" tabIndex="0">
+					{getUserName()}
+					<span className="subheading">Czego chcesz się dzisiaj nauczyć?</span>
+				</h1>
+				<h2 className="tertiary-heading">Przedmioty</h2>
+				<article className="subject-tiles">
+					<Tile name="Polski" />
+					<Tile name="Matematyka" />
+					<Tile name="Angielski" />
+				</article>
+				<h2 className="tertiary-heading">Zagadnienia z ostatniego przedmiotu</h2>
 				{displayTopics(getSubject())}
-			<Menu />
-		</article>
+				<Menu />
+			</article>
+		}</>
 	);
 }
 
